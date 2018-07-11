9b9697b3777d01586a2d9454b6da7f438c022ced
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-07-11 09:33:51 +0100

    launch intent fixed!

--- lambda_data/index.js
+++ lambda_data/index.js
@@ -2,15 +2,23 @@
 const Alexa = require('alexa-sdk');
 const ssmlMediumBreak = "<break time = '0.3s'/>";
 
+var quizLoc = 0;
+var quizScore = 0;
+
+var quizQuestions = {1: "Am I working?"};
+var haveAskedQuestions = false;
+
+var quizAnswers = {1 : {a: "yes", b: "no", c: "maybe"}};
+var currentAnswerId = 0;
+
 
 var handlers = {
 
-    'LaunchRequest': function () {
-        this.response.speak("Hi! Welcome to the Big quiz! <break time = '0.3s' /> \
-         This program is designed to test interactions with alexa in a quiz format <break time = '0.3s' /> \
-          To get instructions <break time = '0.15s' />  say Alexa ask Big Quiz for help <break time = '0.15s' />  \
-          or to start the quiz say Alexa <break time = '0.15s' />  ask big quiz to start quiz");
-        this.emit(':responseReady');
+    'LaunchIntent': function() {
+
+        this.response.speak("Hi! Welcome to Big Quiz! You can ask for help or you can say Alexa, Ask Big Quiz for to start quiz ").listen("If you're not sure <break time = '0.15s' />  just ask for help by saying <break time = '0.15s' />  Alexa ask big quiz for help! ");
+        this.emit(":responseReady");
+        
     },
 
     'HelloIntent': function () {
@@ -28,6 +36,53 @@ var handlers = {
         this.response.speak("Bye from Big Quiz!");
         this.emit(":responseReady");
     },
+
+    'StartQuizIntent': function(){
+        quizLoc = 0;
+        quizScore = 0;
+
+        this.response.speak("I will now set the quiz for you to be tested! <break time = '0.30s' /> Ask me for a question when you'd like to start").listen("You can say <break time = '0.15s' /> Alexa ask big quiz for the next question!");
+        this.emit(":responseReady");
+    },
+
+    'NextQuestionIntent' : function(){
+        quizLoc +=1;
+        haveAskedQuestions = true;
+        this.response.speak("The next question is <break time = '0.4s'/> " + quizQuestions[quizLoc] + "and your choices are : a <break time = '0.3s' />  " + quizAnswers[quizLoc].a + " <break time = '0.15s' />  b <break time = '0.3s' />  " + quizAnswers[quizLoc].b + " <break time = '0.15s' />  and c  <break time = '0.3s' /> \
+         " + quizAnswers[quizLoc].c + "<break time = '0.15s' />  you can answer the question right now <break time = '0.15s' />  or tell me when you're ready <break time = '0.3s' />" ).listen();
+        this.emit(":responseReady");
+    },
+
+    'AnswerQuestionIntent': function(){
+        if(haveAskedQuestions === true){
+            console.log(this); 
+            var answer = request.intent.slots.this.value;
+            if(quizLoc === 1){
+                if(answer === "c"){
+                    this.response.speak("You are right! ");
+                    this.emit(":responseReady");
+                }
+                else {
+                    this.response.speak("You are wrong");
+                    this.emit(":responseReady");
+                }
+            }
+        }
+        else
+        {
+            this.response.speak("The question hasn't been asked I think <break time = '0.15s' />  maybe you should say <break time = '0.3s' />  Alexa Ask Big Quiz for the next question <break time = '0.3s' /> ");
+            this.emit(":responseReady");
+        }
+        this.response.speak("The question hasn't been asked I think <break time = '0.15s' />  maybe you should say <break time = '0.3s' />  Alexa Ask Big Quiz for the next question <break time = '0.3s' /> ");
+        this.emit(":responseReady");
+    },
+
+    'Unhandled': function(){
+        this.response.speak(" This request does not have any code associated with it! ");
+        this.emit(":responseReady");
+    }
+
+    
 
 };
 
